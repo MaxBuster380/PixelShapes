a72532a8757c5138c85a1deaa30414bdd57652c7
MaxBuster380 <maxbuster380@gmail.com>
2023-09-11 10:32:15 +0200

    SetShape containsExcludesRemovedPoints test added

--- src/test/kotlin/pixelshapes/mutablepixelshapes/SetShapeTest.kt
+++ src/test/kotlin/pixelshapes/mutablepixelshapes/SetShapeTest.kt
@@ -1,7 +1,9 @@
 package pixelshapes.mutablepixelshapes
 
+import kotlin.math.abs
 import kotlin.random.Random
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class SetShapeTest {
@@ -19,15 +21,51 @@ class SetShapeTest {
     fun containsKeepsAddedPoints() {
         val shape = SetShape()
 
+        // Add a bunch of points to the shape
         val points = mutableListOf<Pair<Int, Int>>()
-        for(i in 1..100) {
+        for(i in 1..RANDOM_CHECKS_COUNT) {
             val new = randomPoint()
             shape.add(new)
             points += new
         }
 
+        // Check if they are all included in the shape
         for(point in points) {
             assertTrue(shape.contains(point))
+        }
+    }
+
+    /**
+     * Checks if a removed point gives Fale when running .contains
+     */
+    @Test
+    fun containsExcludesRemovedPoints() {
+        val NB_POINTS_REMOVED = RANDOM_CHECKS_COUNT / 2
+
+        val shape = SetShape()
+
+        // Add a bunch of points to the set
+        // "Status" is an external check for if the point remains in the shape.
+        val points = mutableListOf<Pair<Int, Int>>()
+        val pointStatus = mutableListOf<Boolean>()
+        for(i in 1..RANDOM_CHECKS_COUNT) {
+            val new = randomPoint()
+            shape.add(new)
+            points += new
+            pointStatus += true
+        }
+
+        // Remove at most NB_POINTS_REMOVED points
+        for(i in 1..NB_POINTS_REMOVED) {
+            val indexToExclude = abs(Random.nextInt()) % points.size
+
+            shape.remove(points[indexToExclude])
+            pointStatus[indexToExclude] = false
+        }
+
+        // Check that all the points' status match
+        for(i in points.indices) {
+            assertEquals(shape.contains(points[i]), pointStatus[i])
         }
     }
 
