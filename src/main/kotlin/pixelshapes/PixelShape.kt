b167f9dc6d02e1c100e97ec3b8263ce041780753
MaxBuster380 <maxbuster380@gmail.com>
2023-09-11 10:06:34 +0200

    Added contains method to PixelShape

--- src/main/kotlin/pixelshapes/PixelShape.kt
+++ src/main/kotlin/pixelshapes/PixelShape.kt
@@ -4,5 +4,10 @@ package pixelshapes
  * Set of integer coordinates.
  */
 interface PixelShape {
-
+    /**
+     * Checks if the point is inside the shape.
+     * @param point (X, Y) coordinates to check for.
+     * @return True only if a given coordinate is a part of the shape.
+     */
+    fun contains(point : Pair<Int, Int>) : Boolean
 }
