6ee76e50f8ab278438f831b33ca16120fcfa1950
MaxBuster380 <maxbuster380@gmail.com>
2023-09-11 10:11:00 +0200

    MutablePixelShape add and remove methods added for points

--- src/main/kotlin/pixelshapes/mutablepixelshapes/MutablePixelShape.kt
+++ src/main/kotlin/pixelshapes/mutablepixelshapes/MutablePixelShape.kt
@@ -6,4 +6,15 @@ import pixelshapes.PixelShape
  * Set of integer coordinates. Can be modified.
  */
 interface MutablePixelShape : PixelShape {
+    /**
+     * Adds a point to the shape.
+     * @param point (X, Y) coordinate to include in the shape.
+     */
+    fun add(point : Pair<Int, Int>)
+
+    /**
+     * Removes a point from the shape.
+     * @param point (X, Y) coordinate to exclude from the shape.
+     */
+    fun remove(point : Pair<Int, Int>)
 }
